21b4650d33ac363650ab1cf74aa72b54c45bba74
Shaleen Govil <shaleengovil2000@gmail.com>
2020-05-15 15:28:48 +0530

    Create sitting or standing.cpp

---
+++ prerequisites/sitting or standing.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // freeopen("input.txt","r",stdin);
+    // freeopen("output.txt","w",stdout);
+    
+    int r,s;
+    cin >> r >> s;
+    int tickets;
+    cin >> tickets;
+    if(tickets > r*s) {
+        cout << r*s << " " << tickets - r*s ;
+    }
+    else{
+        cout << tickets << " " << "0";
+    }
+    
+    
+    return 0;
+}
