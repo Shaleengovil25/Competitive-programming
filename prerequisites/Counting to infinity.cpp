2cf58796d3145108b169425f74431cb8f3f18944
Shaleen Govil <shaleengovil2000@gmail.com>
2020-05-16 13:27:25 +0530

    Create Counting to infinity.cpp

---
+++ prerequisites/Counting to infinity.cpp
@@ -0,0 +1,17 @@
+https://orac.amt.edu.au/cgi-bin/train/problem.pl?set=simple2&problemid=383
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // freeopen("input.txt","r",stdin);
+    // freeopen("output.txt","w",stdout);
+    
+    int n;
+    cin >> n;
+    
+    for(int i=1;i<=n;i++){
+        cout << i << '\n';
+    }
+    return 0;
+}
