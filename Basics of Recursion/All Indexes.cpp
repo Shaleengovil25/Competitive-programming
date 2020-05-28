01aac5a3a6ca0dcc4e6710985a8341745e105cbd
Shaleen Govil <shaleengovil2000@gmail.com>
2020-05-28 20:29:14 +0530

    Create All Indexes.cpp

---
+++ Basics of Recursion/All Indexes.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int allIndexes(int input[],int size,int x,int output[]){
+    if(size == 0){
+        return 0;
+    }
+    int ans = allIndexes(input+1,size-1,x,output);
+    for(int i=0;i<ans;i++){
+        output[i] +=1;
+    }
+    
+    if(input[0] == x){
+        for(int i=size-1;i>=0;i--){
+            output[i+1] = output[i];
+        }
+        output[0] = 0;
+        return ans +1;
+    }
+    else{
+        return ans;
+    }
+    
+}
+
+int main() {
+	// your code goes here
+	int size,x,o[10];
+	cin >> size;
+	int *arr = new int[size];
+	for(int i=0;i<size;i++){
+	    cin >> arr[i];
+	}
+	cin >> x;
+	int d = allIndexes(arr,size,x,o);
+	for(int i=0;i<d;i++){
+	    cout << o[i] << " ";
+	}
+	delete [] arr;
+	return 0;
+}
+
+input:
+5
+1 8 4 9 8
+8
+
+output:
+1 4
