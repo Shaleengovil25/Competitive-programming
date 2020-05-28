5304a13ef62eb32985542b844b67c73ad54a3eed
Shaleen Govil <shaleengovil2000@gmail.com>
2020-05-29 00:02:38 +0530

    Update All Indexes.cpp

--- Basics of Recursion/All Indexes.cpp
+++ Basics of Recursion/All Indexes.cpp
@@ -51,3 +51,36 @@ input:
 
 output:
 1 4
+
+	
+Iterative Approach =>	
+	
+	
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int size,x;
+	cin >> size;
+	int arr[size];
+	for(int i=0;i<size;i++){
+	    cin >> arr[i];
+	}
+	cin >> x;
+	int count = 0;
+	for(int i=0;i<size;i++){
+	    if(arr[i] == x){
+	        cout << i << " ";
+	    }
+	    else{
+	        count ++;
+	    }
+	}
+	
+	if(count == size){
+	    cout << "0";
+	}
+	return 0;
+}
+	
