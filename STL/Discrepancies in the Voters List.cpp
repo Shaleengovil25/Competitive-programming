c137a804a4a1d114c69f6f3dfb61c3f3f045d98f
Shaleen Govil <shaleengovil2000@gmail.com>
2020-06-05 00:08:41 +0530

    Create Discrepancies in the Voters List.cpp

---
+++ STL/Discrepancies in the Voters List.cpp
@@ -0,0 +1,34 @@
+//APPROACH 1 USING MAP ->
+//Time O(n) Space O(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n1,n2,n3;
+	cin >> n1 >> n2 >> n3;
+	int n = n1+n2+n3;
+	int *arr = new int[n];
+	for(int i=0;i<n;i++){
+	    cin >> arr[i];
+	}
+	map<int,int> m;
+	for(int i=0;i<n;i++){
+	    m[arr[i]]++;
+	}
+	map<int,int>::iterator it;
+	vector<int> o;
+	for(it=m.begin();it!=m.end();it++){
+	    if(it->second !=1){
+	        o.push_back(it->first);
+	    }
+	}
+	cout << o.size() << endl;
+	for(int i=0;i<o.size();i++){
+	    cout << o[i] << endl;
+	}
+	
+	delete [] arr;
+	return 0;
+}
+
